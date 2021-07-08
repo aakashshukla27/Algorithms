ab968507390f0d5edbf42767585f496fd76fa4c9
Aakash Shukla <shukla.aa@northeastern.edu>
2021-07-08 19:50:14 -0400

    Added Graphs

---
+++ Graphs/Helper/AdjacencyList.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs.Helper
+{
+    class AdjacencyList
+    {
+        private int size;
+        LinkedList<int>[] adjList;
+       
+
+        public AdjacencyList(int n)
+        {
+            adjList = new LinkedList<int>[n];
+            size = n;
+            for (int i = 0; i < n; i++)
+            {
+                adjList[i] = new LinkedList<int>();               
+            }
+        }
+        public void addEdge(int u, int v)
+        {
+            adjList[u].AddLast(v);
+            //adjList[v].AddLast(u);
+        }
+
+        public void printGraph()
+        {
+            for(int i=0; i<adjList.Length; i++)
+            {
+                Console.WriteLine("Adjacency List of vertex " + i);
+                Console.Write("head");
+                foreach(var item in adjList[i])
+                {
+                    Console.Write(" -> " + item);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void BreadthFirstSearch(int s)
+        {
+            bool[] visited = new bool[size];
+            for (int i = 0; i < size; i++)
+            {
+                visited[i] = false;
+            }
+
+            Console.WriteLine("BFS of graph is:\n");
+            Queue<int> queue = new Queue<int>();
+            visited[s] = true;
+            queue.Enqueue(s);
+            
+            while(queue.Any())
+            {
+                s = queue.Dequeue();
+                Console.Write(s + " ");
+                LinkedList<int> list = adjList[s];
+                foreach (var item in list)
+                {
+                    if (!visited[item])
+                    {
+                        visited[item] = true;
+                        queue.Enqueue(item);
+                    }
+                }
+
+            }
+        }
+
+        public void DepthFirstSearch(int s)
+        {
+
+        }
+
+    }
+}
---
+++ Graphs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graphs.Helper;
+
+namespace Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AdjacencyList adj = new AdjacencyList(5);
+            adj.addEdge(0, 1);
+            adj.addEdge(0, 4);
+            adj.addEdge(1, 2);
+            adj.addEdge(1, 3);
+            adj.addEdge(1, 4);
+            adj.addEdge(2, 3);
+            adj.addEdge(3, 4);
+
+            adj.printGraph();
+            adj.BreadthFirstSearch(2);
+            Console.ReadKey();
+        }
+    }
+}
